16fb22d5467f99f8041ad3761fcba5969bcee8f0
Taha Ratlam Wala <56840043+taharatlam@users.noreply.github.com>
2024-12-23 16:51:33 +0530

    phone code static

--- src/components/CountryFlagPicker.jsx
+++ src/components/CountryFlagPicker.jsx
@@ -1,24 +1,38 @@
 'use client'
 import React from 'react'
 import ReactFlagsSelect from 'react-flags-select'
+import Image from 'next/image'
+import flagIcon from '@/images/ae.png'
 
 export const CountryFlagPicker = () => {
     const [selected, setSelected] = React.useState('AE')
 
   const phoneCodeMap = {
     'AE': '+971',
-    'US': '+1',
+    'US': '+1', 
     'GB': '+44',
     'IN': '+91'
   }
   return (
-    <ReactFlagsSelect
-        selected={selected}
-        onSelect={code => setSelected(code)}
-        countries={["AE", "US", "GB", "IN"]}
-        customLabels={phoneCodeMap}
-        placeholder="Select"
-        searchable={false}
-    />
+    // <ReactFlagsSelect
+    //     selected={selected}
+    //     onSelect={code => setSelected(code)}
+    //     countries={["AE", "US", "GB", "IN"]}
+    //     customLabels={phoneCodeMap}
+    //     placeholder="Select"
+    //     searchable={false}
+    //     menuShouldScrollIntoView={false}
+    //     menuPosition="fixed"
+    //     appendTo={document.body}
+    // />
+
+        <>
+        <div className="phone-code-static-input">
+          <Image src={flagIcon} alt="Flag" />
+          <span>+971</span>
+          {/* <input type="text" placeholder="Select" /> */}
+        </div>
+        </>
+
   )
 }
